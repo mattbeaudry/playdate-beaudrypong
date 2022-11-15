73f2ca6bdef93413bcabe96662693a801e4db3b9
Matthew Beaudry <matthewbeaudry@Matthews-MBP-2.hitronhub.home>
2022-11-15 18:37:49 -0500

    layout and ui adjustments, new background, coworker graphics

--- source/main.lua
+++ source/main.lua
@@ -7,14 +7,13 @@ local debug = false
 
 -- TODO
 -- split into multiple files/classes
--- add title screen and game start / game over logic
--- track timing of player swing to calculate spin factor + or -
--- ... or use vertical alignment instead, you gotta get above or below the ball to counter spin
+-- gameState object?
+-- calculate spin factor + or - (top or bottom spin) 
+-- .. use vertical alignment, you gotta get above or below the ball to counter spin
 -- what if the players and table are a smily face of the playdate itself, optical illusion
--- ... and the secret is to undock the crank to bring character alive
 -- Smash type swing that requires a pause and using the crank to do a smash
--- how can i implement strength factor to the player and ai swings
 -- ball should go into AI or player hand depending on who won last show
+-- coworker movement and serve functionality
 
 local gfx <const> = playdate.graphics
 local playerSprite = nil
@@ -27,8 +26,6 @@ local playerStand = gfx.image.new("images/player-stand-2")
 local playerSwing = gfx.image.new("images/player-swing-2")
 local playerServe = gfx.image.new("images/player-serve-2")
 local playerThrow = gfx.image.new("images/player-throw-2")
-local playerCanSmash = false
-local aiCanSmash = false
 local coworkerStand = gfx.image.new("images/coworker-stand")
 local coworkerSwing = gfx.image.new("images/coworker-swing")
 local coworkerServe = gfx.image.new("images/coworker-serve")
@@ -37,7 +34,7 @@ local ball = gfx.image.new("images/ball-outline")
 local table = gfx.image.new("images/table")
 local backgroundBorder = gfx.image.new("images/background")
 local backgroundOffice = gfx.image.new("images/background-office")
-local backgroundDepartments = gfx.image.new("images/background-departments")
+local backgroundDepartments = gfx.image.new("images/background-departments-edge")
 
 local gameState = "title"
 local tableEdgeLeft = 110
@@ -67,8 +64,8 @@ local function resetTimer()
 end
 
 local function resetSprites()
-	playerSprite:moveTo(55, 150)
-	player2Sprite:moveTo(345, 150)
+	playerSprite:moveTo(50, 170)
+	player2Sprite:moveTo(350, 170)
 	tableSprite:moveTo(200, 190)
 	ballSprite:moveTo(420, 300)
 end
@@ -90,6 +87,12 @@ local function resetGame()
 	time = 0
 	seconds = 0
 	score = {0, 0}
+end
+
+local function resetScreen() 
+	gfx.setBackgroundColor(gfx.kColorWhite)
+	gfx.setColor(gfx.kColorWhite)
+	gfx.fillRect(0, 0, 400, 240)
 end
 
 local function playerSwings()
@@ -152,7 +155,7 @@ local function playerServes()
 		end)
 	else
 		print("player serving")
-		ballSprite:moveTo(98, playerSprite.y)
+		ballSprite:moveTo(playerSprite.x + 38, playerSprite.y - 20)
 		playerSprite:setImage(playerServe)
 		playerServing = true
 	end
@@ -315,7 +318,7 @@ end
 
 local function endScreen()
 	gfx.drawText("Game Over", 150, 60)
-	gfx.drawText("press A to rematch", 20, 100)
+	gfx.drawText("Score: XXX      press A to restart", 20, 100)
 end
 
 function playdate.update()
@@ -325,26 +328,23 @@ function playdate.update()
 			gameState = "mission"
 		end
 	elseif gameState == "mission" then
-		gfx.sprite.removeAll()
-		gfx.sprite.update()
-		initialize()
+		resetScreen()
 		missionScreen()
 		if playdate.buttonJustPressed(playdate.kButtonA) then
 			gameState = "play"
+			initialize()
 		end
 	elseif gameState == "end" then
 		gfx.sprite.removeAll()
 		gfx.sprite.update()
+		resetScreen()
 		endScreen()
 		if playdate.buttonJustPressed(playdate.kButtonA) then
 			gameState = "title"
 			gfx.sprite.removeAll()
 			gfx.sprite.update()
-			initialize()
-			
 		end
 	elseif gameState == "play" then
-		
 		-- Controls
 		if playdate.buttonJustPressed(playdate.kButtonA) then
 			if ballMoving then
@@ -386,14 +386,14 @@ function playdate.update()
 		-- end
 		
 		-- UI
-		gfx.drawText(score[1], 55, 180)
-		gfx.drawText(score[2], 340, 180)
-		gfx.drawText("SPIN " .. ballSpin, 160, 180)
+		gfx.drawText(score[1], 55, 220)
+		gfx.drawText(score[2], 340, 220)
+		gfx.drawText("SPIN " .. ballSpin, 160, 220)
 		
 		if playerCanSmash then
-			gfx.drawText("SMASH", 100, 180)
+			gfx.drawText("SMASH", 100, 220)
 		elseif aiCanSmash then
-			gfx.drawText("SMASH", 300, 180)
+			gfx.drawText("SMASH", 300, 220)
 		end
 		
 		if debug == false then
@@ -402,7 +402,7 @@ function playdate.update()
 			
 		-- On-screen messages
 		if showMessage then
-			gfx.drawText("POINT!", 200, 170)
+			gfx.drawText("POINT!", 200, 200)
 			playdate.timer.performAfterDelay(1000, function()
 				showMessage = false
 			end)
